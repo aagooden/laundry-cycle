11c791d295c2b883dc64842406df642f42ae3f30
Aaron Gooden <aagooden@me.com>
2018-02-07 21:49:07 -0500

    added volume control function

--- Ruby/songs.rb
+++ Ruby/songs.rb
@@ -1,34 +1,54 @@
-live_loop :midi do
-  button, parameter = sync "/midi/nanokontrol_slider_knob/0/1/control_change"
-  if button == 45
-    harry_potter
-  end
-end
 
-def harry_potter
+
+def harry_potter(speed)
   tune = [[60,0.5], [65, 0.75], [68,0.25], [67, 0.5], [65,1], [72,0.5],[70,1.25],[67, 1.5],
           [65, 0.75], [68,0.25], [67, 0.5], [64,1],[68, 0.5], [60,1],
-          [60,0.5], [65, 0.75], [68,0.25], [67, 0.5], [65,1], [72, 1], [75, 0.5], [74, 0.5], [73, 0.5],
-          [68, 1], [72, 1], [70, 1], [65, 1], [68,1], [65,1]]
+          [60,0.5], [65, 0.75], [68,0.25], [67, 0.5], [65,1], [72, 0.5], [75, 1], [74, 0.5], [73, 1],
+          [68, 0.5], [72, 0.5], [70, 1], [65, 1], [68,1], [65,1]]
   
   tune.length.times do |x|
-    speed = parameter / 127
     pitch = tune[x][0]
     time = (tune[x][1]) * speed
-    synth :pretty_bell, note: pitch, amp: 30, release: time
+    volume = volume_control()
+    synth :pretty_bell, note: pitch, release: time + 0.75, attack: 0.25, amp: volume
     sleep time
   end
 end
 
-def jeopardy()
+def jeopardy(speed)
   tune = [[65, 1], [70, 1], [65,1], [58, 1], [65,1], [70,1],[65,2],
           [65, 1], [70, 1], [65,1]]
   x = 0
   while x < tune.length
     pitch = tune[x][0]
-    time = tune[x][1]
-    synth :hollow, note: pitch, amp: 30, release: time
+    time = tune[x][1] * speed
+    volume = volume_control()
+    synth :piano, note: pitch, amp: volume, release: time + 1
+    synth :hollow, note: pitch, amp: volume, release: time + 0.5
     sleep time
     x = x + 1
   end
+end
+
+def volume_control()
+  knob, amount = get "/midi/nanokontrol_slider_knob/0/1/control_change"
+  if knob == 14 || knob == 15
+    volume = (amount + 5) / 5
+  else
+    volume = 1
+    return volume
+  end
+end
+
+loop do
+  button = 0
+  live_loop :midi do
+    button, active = sync "/midi/nanokontrol_slider_knob/0/1/control_change"
+    if button == 23
+      harry_potter(0.9)
+    elsif button == 24
+      jeopardy(0.5)
+    end
+  end
+  sleep 0.25
 end
