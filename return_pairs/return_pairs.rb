bb869c4a6ee77f9c062ab7c8c4db917580c2a23e
Aaron Gooden <aagooden@me.com>
2018-03-01 16:42:24 -0500

    Added test to return pairs as nested arrays and passed

--- return_pairs/return_pairs.rb
+++ return_pairs/return_pairs.rb
@@ -1,4 +1,40 @@
-def return_pairs()
+def shuffle(people)
+	people = people.shuffle
+	print people
+	puts ""
+	return people
+end
+
+def return_pairs(people)
+	people = shuffle(people)
 	pairs_array = []
+	num = people.length / 2
+	for x in (0...num)
+		pairs_array.push(people.slice(x * 2, 2))
+	end
+	
+	print pairs_array
 	return pairs_array
 end
+
+#return_pairs(["a", "b", "c", "d", "e", "f", "z"])
+
+
+
+
+
+# def return_pairs(people)
+# 	people = people.shuffle
+# 	pairs_array = []
+
+# 	for x in (people.length)
+# 		for y in (0..1)
+# 		pairs_array[]
+# 		end
+# 	end
+# 		pairs_array.push(x)	
+# 	end
+# 	print pairs_array
+
+# 	return pairs_array
+# end
--- return_pairs/test_return_pairs.rb
+++ return_pairs/test_return_pairs.rb
@@ -8,12 +8,27 @@ class Addition_test < Minitest::Test
 	end
 
 	def test_for_returned_array
-		pairs = return_pairs()
+		pairs = return_pairs(["Fred", "Wilma"])
 		assert_equal(Array, pairs.class)
 	end
+
+	def test_for_returning_one_pair_when_sent_two_names
+		pairs = return_pairs(["Betty", "Boop"])
+		assert_equal(pairs.length, 1)
+	end
+
+	def test_for_returning_two_pairs_in_nested_arrays_when_sending_four_names
+		pairs = return_pairs(["Jack", "Jill", "Bob", "Builder"])
+		assert_equal(pairs[0].length, 2)
+		assert_equal(pairs[1].length, 2)
+	end
+
+	def test_for_randomness
+		random = shuffle(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"])
+		if random != ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+			condition = true
+		end
+		assert_equal(condition, true)
+	end
+
 end
-	# def test_four_numbers
-	# 	num1 = "1234"
-	# 	num2 = "1234"
-	# 	number = off_by_one(num1, num2)
-	# 	assert_equal(4, number)
